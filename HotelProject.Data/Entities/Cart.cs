de1d79e21e218a97b8c8f79c6ee11e81fa424cf8
Cem Kara <cemkara1992@gmail.com>
2023-08-28 11:03:36 +0300

    card and carditem classes added.

---
+++ HotelProject.Data/Entities/Cart.cs
@@ -0,0 +1,17 @@
+public class Cart
+{
+        public int CartId { get; set; }
+        public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in CartItems)
+                {
+                    totalPrice += item.Product.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+    }
+}
---
+++ HotelProject.Data/Entities/CartItem.cs
@@ -0,0 +1,6 @@
+public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
